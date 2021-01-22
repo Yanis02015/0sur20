710e79e942b5b5a637364766bb2101dc08007c42
Yanis02015 <yaniscrab@gmail.com>
2021-01-23 00:43:18 +0100

    JS End + CSS Flex Grid

--- script.js
+++ script.js
@@ -1,19 +1,177 @@
-var i = 0;
+var iMenu = 0;
 function openNav() {
     var a = document.getElementById("nav");
-    if(i === 1){
+    if(iMenu === 1){
         a.style.paddingLeft = "0px";
         a.style.paddingRight = "0px";
         a.style.width = "0px";
-        i=0;
+        iMenu=0;
     } else{
         a.style.paddingLeft = "15px";
         a.style.width = "180px";
-        i=1;
+        iMenu=1;
     }
-  }
+}
 
-  function sauvgarder() {
-    var a = document.getElementById("number");
-    a.textContent = "00.01"
+// Véréfier si tous les champ ont était rempli
+var iFromValidity = 0;
+function fromValidation(){
+    var fromValidity = document.forms["post-form"].checkValidity();
+    if(fromValidity){
+        calculate();
+    }else{ //Juste une alerte après chaque 10 essais foireux
+        iFromValidity++;
+        if(iFromValidity%9 === 0){
+            alert("Merci de bien vouloir remplir tous les champs");
+        }
+    }
+    
+}
+
+// Calculer la moyenne
+var i = 0;
+function calculate() {
+    var moy = 0;
+    var totalCoef = 0;
+    for(var j=0; j <= i;j++){
+        var emd = document.getElementById("emd-"+j).value;
+        var td = document.getElementById("td-"+j).value;
+        var tp = document.getElementById("tp-"+j).value;
+        var coef = document.getElementById("coef-"+j).value;
+        emd = parseFloat(emd);
+        td = parseFloat(td);
+        tp = parseFloat(tp);
+        coef = parseFloat(coef);
+        totalCoef += coef;
+        moy += choice(emd,td,tp)*coef;
+        console.log(choice(emd,td,tp)*coef);
+    }
+    moy /= totalCoef;
+    moy += 0.004;
+    moy = moy.toFixed(2);
+    moy = moy.toString().padStart(5, '0');
+    var Average = document.getElementById("number");
+    Average.textContent = moy;
+}
+function choice(a,z,e) {
+    if(z!==-1 && e !==-1){
+        moy1 = a*0.6 + (z/2 + e/2)*0.4;
+        moy2 = a*2 + z + e;
+        moy2 /= 4;
+    } else if(z == -1 && e !==-1) {
+            moy1 = a*0.6 + e*0.4;
+            moy2 = a*2 + e;
+            moy2 /= 3;
+        }else if(e == -1 && z !==-1) {
+            moy1 = a*0.6 + z*0.4;
+            moy2 = a*2 + z;
+            moy2 /= 3;
+            }else {
+                moy1 = a;
+                moy2 = a;
+            }
+    if(moy1>=moy2){
+        return moy1;
+    }else{
+        return moy2;
+    }
+}
+
+// L'ajout de ligne d'input
+function add() {
+    i=i+1;
+    var container = document.getElementsByClassName("input-bottom");
+    var line = document.createElement("div");
+    line.className = "line";
+
+    var emdName = document.createElement("div");
+    emdName.className = "emd-name";
+
+    var divEmd = document.createElement("div");
+    var divBreak = document.createElement("break");
+    divBreak.className = "break";
+    divBreak.style.width = "0px"
+    var divTd = document.createElement("div");
+    var divTp = document.createElement("div");
+    var divCoef = document.createElement("div");
+    divCoef.setAttribute("id","coef");
+
+    var inputName = document.createElement("input");
+    inputName.setAttribute('id',"name-"+i);
+    inputName.setAttribute('required',"required");
+    inputName.type = "text";
+    inputName.placeholder = "Set Name";
+    inputName.removeAttribute('autocomplete');
+
+    var inputEmd = document.createElement("input");
+    inputEmd.setAttribute('id',"emd-"+i);
+    inputEmd.setAttribute('required',"required");
+    inputEmd.setAttribute('step',"0.01");
+    inputEmd.max="20";
+    inputEmd.min="-1";
+    inputEmd.type = "number";
+    inputEmd.placeholder = "EMD";
+
+    var inputTd = document.createElement("input");
+    inputTd.setAttribute('id',"td-"+i);
+    inputTd.setAttribute('required',"required");
+    inputTd.setAttribute('step',"0.01");
+    inputTd.max="20";
+    inputTd.min="-1";
+    inputTd.type = "number";
+    inputTd.placeholder = "TD";
+
+    var inputTp = document.createElement("input");
+    inputTp.setAttribute('id',"tp-"+i);
+    inputTp.setAttribute('required',"required");
+    inputTp.setAttribute('step',"0.01");
+    inputTp.max="20";
+    inputTp.min="-1";
+    inputTp.type = "number";
+    inputTp.placeholder = "TP";
+
+    var inputCoef = document.createElement("input");
+    inputCoef.setAttribute('id',"coef-"+i);
+    inputCoef.setAttribute('required',"required");
+    inputCoef.max="20";
+    inputCoef.min="1";
+    inputCoef.type = "number";
+    inputCoef.value = "2";
+
+    emdName.appendChild(inputName);
+    divEmd.appendChild(inputEmd);
+    divTd.appendChild(inputTd);
+    divTp.appendChild(inputTp);
+    divCoef.appendChild(inputCoef);
+
+    line.appendChild(emdName);
+    line.appendChild(divEmd);
+    line.appendChild(divBreak);
+    line.appendChild(divTd);
+    line.appendChild(divTp);
+    line.appendChild(divCoef);
+    
+    container[0].appendChild(line);
+    check();
+}
+
+// Supprimer la derniere ligne ajouter
+function delet() {
+    if(i>0){
+        var line = document.getElementsByClassName("line");
+        var container = document.getElementsByClassName("input-bottom");
+        container[0].removeChild(line[i]);
+        i -= 1;
+    }
+    check();
+}
+
+function check() {
+    var delet = document.getElementById("btn-delet");
+    if(i<=0){
+        delet.style.cursor = "not-allowed";
+    } else{
+        delet.style.display = "inline";
+        delet.style.cursor = "pointer";
+    }
 }
